a8c0c969fc0f9f57fd35526e3a87117b23eed304
jerrylchong <65395269+jerrylchong@users.noreply.github.com>
2024-11-01 13:33:15 +0800

    Update config.h

--- keyboards/bastardkb/tbkmini/v1/elitec/config.h
+++ keyboards/bastardkb/tbkmini/v1/elitec/config.h
@@ -25,8 +25,9 @@
     { B4, E6, C6, B1, B3, B2 }
 
 /* Handedness. */
-#define MASTER_RIGHT
+#define MASTER_LEFT
 #define FORCE_NKRO
+#define PERMISSIVE_HOLD
 
 /* RGB settings. */
 #define RGB_DI_PIN D2
